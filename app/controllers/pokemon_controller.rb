e182809e992459b906726fdb2ef33737d535a7e5
Dominique McCloud <niquelejon@gmail.com>
2017-10-10 19:26:14 +0000

    fixed indentation

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,25 +1,25 @@
 class PokemonController < ApplicationController
 
    	get '/pokemon' do
-   		if logged_in?
-        @trainers = Trainer.all
-        @trainer = @trainers.find_by_id(current_user.id)
-        @pokemons = @trainer.pokemon
-   			erb :'/pokemon/index'
-   		else
-   			redirect to '/login'
-   		end
+     		if logged_in?
+          @trainers = Trainer.all
+          @trainer = @trainers.find_by_id(current_user.id)
+          @pokemons = @trainer.pokemon
+     			erb :'/pokemon/index'
+     		else
+     			redirect to '/login'
+     		end
    	end
 
    	get '/pokemon/new' do
-   		if logged_in?
-        @trainers = Trainer.all
-        @trainer = @trainers.find_by_id(current_user.id)
-        @pokemons = @trainer.pokemon
-   			erb :'pokemon/create_pokemon'
-   		else
-   			redirect to '/login'
-   		end
+     		if logged_in?
+          @trainers = Trainer.all
+          @trainer = @trainers.find_by_id(current_user.id)
+          @pokemons = @trainer.pokemon
+     			erb :'pokemon/create_pokemon'
+     		else
+     			redirect to '/login'
+     		end
    	end
 
     post '/pokemon/new' do
@@ -29,8 +29,7 @@ class PokemonController < ApplicationController
           @pokemon.trainer_id = current_user.id
           @pokemon.save
           redirect to "/pokemon/home"
-
-      end
+    end
 
 
    	get '/pokemon/home' do
@@ -47,30 +46,29 @@ class PokemonController < ApplicationController
 
     get '/pokemon/:id/edit' do
       if logged_in?
-        @trainers = Trainer.all
-        @trainer = @trainers.find_by_id(current_user.id)
-        @pokemons = @trainer.pokemon
-        @pokemon = @pokemons.find(params[:id])
-        if current_user.id == @pokemon.trainer_id
-         erb :'pokemon/edit_pokemon'
-        else
-          redirect to '/pokemon/home'
-        end
-      else
-        redirect to '/login'
-      end
+          @trainers = Trainer.all
+          @trainer = @trainers.find_by_id(current_user.id)
+          @pokemons = @trainer.pokemon
+          @pokemon = @pokemons.find(params[:id])
+            if current_user.id == @pokemon.trainer_id
+              erb :'pokemon/edit_pokemon'
+            else
+              redirect to '/pokemon/home'
+            end
+       else
+         redirect to '/login'
+       end
     end
 
     patch '/pokemon/:id' do
-        @trainers = Trainer.all
-        @trainer = @trainers.find_by_id(current_user.id)
-        @pokemons = @trainer.pokemon
+       @trainers = Trainer.all
+       @trainer = @trainers.find_by_id(current_user.id)
+       @pokemons = @trainer.pokemon
        @pokemon = @pokemons.find_by_id(params[:id])
        @pokemon.name = params[:name]
        @pokemon.save
        redirect to "/pokemon/home"
-
-   end
+     end
 
 
    	delete '/pokemon/:id/delete' do
@@ -83,7 +81,7 @@ class PokemonController < ApplicationController
    		elsif current_user.id == @pokemon.trainer_id
    			@pokemon.destroy
    		end
-
    		redirect to '/pokemon/home'
    	end
-   end
+
+  end #ends class
--- app/controllers/trainer_controller.rb
+++ app/controllers/trainer_controller.rb
@@ -4,62 +4,63 @@ class TrainersController < ApplicationController
   #displays the user signup
   #only shows the sign-up page if user is not logged in if they are logged in, redirects them to their tweets
     get "/" do
-    if !logged_in?
-      erb :"index"
-    else
-      redirect '/pokemon/home'
+        if !logged_in?
+          erb :"index"
+        else
+          redirect '/pokemon/home'
+        end
     end
-  end
 
   #Trainers submission form is loaded via the POST request to /signup
-    post "/" do
-      if params[:username]== "" || params[:name]== "" || params[:password]== "" || params[:photo]== ""
-       redirect '/'
-     else
-      @trainer = Trainer.new(name: params[:name], username: params[:username], password: params[:password], photo: params[:photo])
-    end
-      if @trainer.save
-        @trainer.save
-        session[:trainer_id] = @trainer.id
-        redirect to "/pokemon/home"
-      else
-      erb :'index', locals: {message: "USERNAME ALREADY EXISTS! Please choose new username"}
-    end
-  end
+    post "/signup" do
+        if params[:username]== "" || params[:name]== "" || params[:password]== "" || params[:photo]== ""
+          redirect '/'
+        else
+          @trainer = Trainer.new(name: params[:name], username: params[:username], password: params[:password], photo: params[:photo])
+        end
+
+        if @trainer.save
+           @trainer.save
+          session[:trainer_id] = @trainer.id
+          redirect to "/pokemon/home"
+        else
+          erb :'index', locals: {message: "USERNAME ALREADY EXISTS! Please choose new username"}
+        end
+     end
 
   #renders the login page for a user
   #doesn't let user view the login page if already logged in"
     get "/login" do
-      if logged_in?
-      redirect "/pokemon/home"
-    else
-      erb :'/trainers/login'
-      end
+        if logged_in?
+          redirect "/pokemon/home"
+        else
+          erb :'/trainers/login'
+        end
     end
 
   #login form is loaded via post /login request
     post "/login" do
-      trainer = Trainer.find_by(:username => params[:username])
-      if trainer && trainer.authenticate(params[:password])
-          session[:trainer_id] = trainer.id
-          redirect to "/pokemon/home"
-      else
-          erb :'/trainers/login', locals: {message: "Please login with valid username/password before continuing"}
-      end
+        trainer = Trainer.find_by(:username => params[:username])
+        if trainer && trainer.authenticate(params[:password])
+            session[:trainer_id] = trainer.id
+            redirect to "/pokemon/home"
+        else
+            erb :'/trainers/login', locals: {message: "Please login with valid username/password before continuing"}
+        end
     end
 
     get '/trainer/:id/edit' do
-      if logged_in?
-        @trainers = Trainer.all
-        @trainer = @trainers.find(params[:id])
-        if current_user.id == @trainer.id
-         erb :'/trainers/edit_trainer'
+        if logged_in?
+          @trainers = Trainer.all
+          @trainer = @trainers.find(params[:id])
+            if current_user.id == @trainer.id
+              erb :'/trainers/edit_trainer'
+            else
+              redirect to '/pokemon/home'
+            end
         else
-          redirect to '/pokemon/home'
+          redirect to '/login'
         end
-      else
-        redirect to '/login'
-      end
     end
 
     patch '/trainer/:id' do
@@ -69,7 +70,7 @@ class TrainersController < ApplicationController
        @trainer.bio = params[:bio]
        @trainer.save
        redirect to "/pokemon/home"
-   end
+    end
 
     get '/logout' do
        if logged_in?
@@ -78,6 +79,6 @@ class TrainersController < ApplicationController
        else
          redirect '/'
        end
-     end
+    end
 
   end #ends class
